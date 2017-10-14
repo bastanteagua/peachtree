7a60027069dec4928db8d4f4c05c878c674ac8f4
bastanteagua <arturoaguero@gmail.com>
2017-10-14 16:46:09 -0500

    gray images

---
+++ components/transactions-list.component.js
@@ -0,0 +1,20 @@
+angular.module('peachtreeApp')
+.filter('timestamp', function() {
+  return function(input) {
+    let date = new Date(input);
+    let month= date.toLocaleString("en-us", { month: "long" });
+    let day= date.getDay();
+    
+    return  month.substr(0,3) + '. ' + day;
+  };
+})
+.component('transactionsList', {
+        templateUrl: '../templates/transactions-list.template.html',
+        controller: function TransactionsListController($http) {
+          var self = this;
+          $http.get('mock/transactions.json').then(function(response) {
+            self.transactions = response.data.data;
+          });
+        },
+    }
+);
